55efa12751140511a9efe96647472020b641d72f
Deviant1407 <144005564+Deviant1407@users.noreply.github.com>
2024-09-05 14:30:59 +0300

    Add files via upload

---
+++ БД.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Сен 05 2024 г., 13:30
+-- Версия сервера: 10.4.32-MariaDB
+-- Версия PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `назушений.нет`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Request`
+--
+
+CREATE TABLE `Request` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `auto_number` varchar(255) NOT NULL,
+  `create_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Role`
+--
+
+CREATE TABLE `Role` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `Role`
+--
+
+INSERT INTO `Role` (`id`, `code`, `name`) VALUES
+(1, 'user', 'Пользователь'),
+(2, 'admin', 'Администратор');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Status`
+--
+
+CREATE TABLE `Status` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `Status`
+--
+
+INSERT INTO `Status` (`id`, `code`, `name`) VALUES
+(1, 'new', 'Новое'),
+(2, 'approve', 'Подтверждено'),
+(3, 'rejcted', 'Отклонено');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `User`
+--
+
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `User`
+--
+
+INSERT INTO `User` (`id`, `role_id`, `username`, `password`, `email`, `first_name`, `last_name`, `middle_name`, `phone`) VALUES
+(1, 2, 'copp', '5f4dcc3b5aa765d61d8327deb882cf99', 'admin@admin.ru', 'Иван', 'Иванов', 'Иванович', '+7(913)-999-99-99');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Request`
+--
+ALTER TABLE `Request`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `status_id` (`status_id`);
+
+--
+-- Индексы таблицы `Role`
+--
+ALTER TABLE `Role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `Status`
+--
+ALTER TABLE `Status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `Request`
+--
+ALTER TABLE `Request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `Role`
+--
+ALTER TABLE `Role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `Status`
+--
+ALTER TABLE `Status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `Request`
+--
+ALTER TABLE `Request`
+  ADD CONSTRAINT `Request_ibfk_1` FOREIGN KEY (`status_id`) REFERENCES `Status` (`id`),
+  ADD CONSTRAINT `Request_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `User`
+--
+ALTER TABLE `User`
+  ADD CONSTRAINT `User_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `Role` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
